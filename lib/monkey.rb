d5a6363c39ac36344bfd9df32fb96a5ee6eb8d56
Uwe Stuehler <uwe@bsdx.de>
2013-05-17 02:28:09 +0200

    Add a generic config mechanism
    The configuration for all modules is read from ~/.monkey/config.yml.

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -1,5 +1,24 @@
 module Monkey
   autoload :Accounting, 'monkey/accounting'
   autoload :Business, 'monkey/business'
+  autoload :Config, 'monkey/config'
   autoload :ProcessMail, 'monkey/process_mail'
+
+  def self.default_config
+    Config.new
+  end
+
+  def self.load_config(filename)
+    Config.load_file filename
+  end
+
+  def self.config_file
+    File.expand_path '~/.monkey/config.yml'
+  end
+
+  def self.config
+    @config ||= File.exists?(config_file) ? load_config(config_file) :
+      default_config
+  end
+
 end
---
+++ lib/monkey/config.rb
@@ -0,0 +1,57 @@
+module Monkey
+
+  class Config
+
+    def self.load_file(filename)
+      Config.new YAML.load_file(filename)
+    end
+
+    def initialize(options = {})
+      options.each_pair do |option, value|
+        if respond_to? "#{option}="
+          send("#{option}=", value)
+        elsif has_section? option
+          section = class_for_section(option).new(value)
+          (class << self; self; end).class_eval do
+            define_method(option) { section }
+          end
+        else
+          raise "invalid configuration option: #{option}"
+        end
+      end
+    end
+
+    def method_missing(method_name, *args)
+      if has_section? method_name
+        section = class_for_section(method_name).new
+        (class << self; self; end).class_eval do
+          define_method(method_name) { section }
+        end
+        section
+      else
+        super
+      end
+    end
+
+    private
+
+    def has_section?(section)
+      class_for_section(section) != nil
+    end
+
+    def class_for_section(section)
+      # TODO: CamelCase the section when capitalization isn't enough
+      section_const = section.capitalize
+      parent_module = Module.nesting[1]
+      if parent_module.const_defined? section_const
+        section_module = parent_module.const_get(section_const)
+        if section_module.const_defined? :Config
+          return section_module.const_get :Config
+        end
+      end
+      nil
+    end
+
+  end
+
+end
