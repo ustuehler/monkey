50db44dd249ea60958e7e60d73178c3a7b23d8aa
Uwe Stuehler <uwe@bsdx.de>
2013-08-13 03:00:38 +0200

    Add per-customer and default hourly rate

--- lib/monkey/business/config.rb
+++ lib/monkey/business/config.rb
@@ -11,6 +11,8 @@ module Monkey::Business
     attr_accessor :default_purchases_account
     attr_accessor :default_input_tax_account
 
+    attr_reader :default_hourly_rate
+
     attr_accessor :ledger_file
 
     def initialize(options = {})
@@ -22,9 +24,15 @@ module Monkey::Business
       @default_purchases_account = 'Expenses:Purchases'
       @default_input_tax_account = 'Assets:Input tax'
 
+      self.default_hourly_rate = '$0'
+
       @ledger_file = nil
 
       super
+    end
+
+    def default_hourly_rate=(value)
+      @default_hourly_rate = Monkey::Accounting::Amount.coerce(value)
     end
   end
 
--- lib/monkey/business/customer.rb
+++ lib/monkey/business/customer.rb
@@ -12,6 +12,7 @@ module Monkey::Business
     property :receivable_account_name, String
     property :sales_account_name, String
     property :tax_account_name, String
+    property :hourly_rate, String
 
     has n, :invoices
 
@@ -27,6 +28,10 @@ module Monkey::Business
       Monkey.config.business.default_tax_account
     end
 
+    def default_hourly_rate
+      Monkey.config.business.default_hourly_rate
+    end
+
     def receivable_account
       Monkey::Business.ledger.account(receivable_account_name || default_receivable_account_name)
     end
@@ -37,6 +42,14 @@ module Monkey::Business
 
     def tax_account
       Monkey::Business.ledger.account(tax_account_name || default_tax_account_name)
+    end
+
+    def hourly_rate
+      Monkey::Accounting::Amount.coerce(super || default_hourly_rate)
+    end
+
+    def hourly_rate=(value)
+      super(Monkey::Accounting::Amount.coerce(value || default_hourly_rate).to_s)
     end
 
   end
