0edc486deb324ad32f86b273c84240fd6e6138df
Uwe Stuehler <uwe@bsdx.de>
2014-02-11 01:39:43 +0100

    Make it easier to load subcommands

--- lib/monkey/commands/business.rb
+++ lib/monkey/commands/business.rb
@@ -6,11 +6,5 @@ invoices (incoming and outgoing), taxes and such things.
 EOS
 
 command :business do |c|
-  path = File.join(File.dirname(__FILE__), 'business')
-  Dir.entries(path).sort.each do |entry|
-    file = File.join(path, entry)
-    if file.end_with?('.rb') and File.file?(file)
-      c.instance_eval File.read(file), file, 1
-    end
-  end
+  c.commands_from 'monkey/commands/business'
 end
